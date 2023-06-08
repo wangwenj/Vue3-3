04af0ca615c21fa3c9a198dbfeb355fd4c6649f6
winnifrede.wang <winnifrede.wang@vipshop.com>
2023-06-09 00:33:58 +0800

    新增store

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    // 在这里定义你的状态
+  },
+  mutations: {
+    // 在这里定义你的 mutation
+  },
+  actions: {
+    // 在这里定义你的 action
+  },
+  modules: {
+    // 在这里定义你的模块
+  }
+})
